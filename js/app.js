5718bb3241baca6f479f6e60bc1af5e59d81adf1
AfnanFerdousi <afnanferdousi550@gmail.com>
2022-02-12 22:20:04 +0600

    done

---
+++ js/app.js
@@ -0,0 +1,63 @@
+function updateProductNumber(product,price,isIncreasing) {
+    const productInput = document.getElementById(product + '-number');
+    let productNumber = productInput.value;    
+    
+    if (isIncreasing == true) {
+        productNumber = parseInt (productNumber) + 1;
+    }
+    else if (isIncreasing == false && productNumber > 0 ){
+        productNumber = parseInt (productNumber) - 1;
+    }
+    productInput.value = productNumber;
+    // update total
+    const productTotal = document.getElementById(product + '-total');
+    productTotal.innerText = productNumber * price;
+
+    // calculate total
+    calulateTotal();
+
+}
+
+function getInputValue(product) {
+    const productInput = document.getElementById(product + '-number');   
+    const productNumber = parseInt(productInput.value); 
+    return productNumber;
+}
+
+function calulateTotal() {
+    const phoneTotal = getInputValue('phone') * 1219;
+    const caseTotal = getInputValue('case') * 59;
+    const subTotal = phoneTotal + caseTotal; 
+    const tax = subTotal * .1;
+    // tax.toFixed(2);
+    const totalPrice = subTotal + tax;
+    // update html
+    document.getElementById('sub-total').innerText = subTotal;
+    document.getElementById('tax-amount').innerText = tax;
+    document.getElementById('total-price').innerText = totalPrice;
+}
+
+// PHONE EVENTS
+
+// add phone
+document.getElementById("phone-plus").addEventListener('click',function () {
+    updateProductNumber('phone',1219,true);
+ })
+
+// minus phone
+document.getElementById("phone-minus").addEventListener('click',function () {
+    updateProductNumber('phone',1219,false);            
+})
+
+
+//  CASE EVENTS
+
+// add case items
+document.getElementById("case-plus").addEventListener('click',function () {
+   updateProductNumber('case',59,true);
+})
+
+// minus case items
+document.getElementById("case-minus").addEventListener('click',function () {
+    updateProductNumber('case',59,false);            
+})
